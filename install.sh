8936660e029f6b1743dfee393c557b44fcec3a1b
irham-s-a <irham.satria.alf@gmail.com>
2026-04-10 11:39:40 +0700

    Update install.sh
    Fix install

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-pkg install -y ffmpeg bc whiptail
+echo "[+] Setting up irh_vconvert..."
 
+# Folder aplikasi
+mkdir -p /sdcard/irh_vconvert/input
+mkdir -p /sdcard/irh_vconvert/output
+
+# Pasang command ke PATH
 cp irh_vconvert.sh $PREFIX/bin/irh_vconvert
 chmod +x $PREFIX/bin/irh_vconvert
 
-echo "======================="
-echo "✅ Install selesai"
+echo "[✓] Install selesai"
 echo "▶ Jalankan: irh_vconvert"
-echo "======================="
