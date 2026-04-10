ca4feabe5afa83f7991beccb4b1c439d2f93bba1
irham-s-a <irham.satria.alf@gmail.com>
2026-04-10 17:45:32 +0700

    Update install.sh
    Fix install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ mkdir -p /sdcard/irh_vconvert/output
 
 # Pasang command ke PATH
 cp irh_vconvert.sh $PREFIX/bin/irh_vconvert
-chmod +x $PREFIX/bin/irh_vconvert
+chmod +x $PREFIX/bin/irh-vconvert
 
 echo "[✓] Install selesai"
-echo "▶ Jalankan: irh_vconvert"
+echo "▶ Jalankan: irh-vconvert"
